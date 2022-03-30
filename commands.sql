01f6876164559506b76dff4daea1a9e74f795e4c
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-03-30 10:55:19 +0200

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -16,31 +16,3 @@ CREATE TABLE notes (
 );
 
 
-class Blogs extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  url: {
-    type: DataTypes.TEXT,
-     allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-     allowNull: false
-  },
-   likes: {
-    type: DataTypes.INTEGER
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
