98e36ff3176e89d58e7b78431186cb80b32ddc1e
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-04-06 18:14:18 +0200

    13.22

--- controllers/readingList.js
+++ controllers/readingList.js
@@ -19,4 +19,20 @@ ReadingListRouter.post("/", tokenExtractor, async (req, res) => {
   }
 })
 
+ReadingListRouter.put("/:id", tokenExtractor, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.decodedToken.id)
+    if (user) {
+      const reading = await ReadingList.findByPk(req.params.id)
+      reading.read = req.body.read
+
+      const modReading = await reading.save()
+      res.json(modReading)
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({ error })
+  }
+})
+
 module.exports = ReadingListRouter
